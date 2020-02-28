9f4b19bd0481b09e832e8a5d9b6fc0c5baef1009
Steven Cassidy <scass0807@gmail.com>
2020-02-27 22:28:59 -0500

    Added first verstion template header content footer color scheme layout

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,12 +6,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
