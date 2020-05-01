437d8f52387620b7ba8c1e0641ad600d73ee1d92
Steven Cassidy <scass0807@gmail.com>
2020-05-01 09:09:19 -0400

    presentation

--- backend/api/app/routes/meeting_data.routes.js
+++ backend/api/app/routes/meeting_data.routes.js
@@ -6,10 +6,10 @@ const auth = require("../auth/auth.js")
 const meeting_data = require("../controllers/meeting_data.controller.js");
 
 //Create meeting_data
-router.post("/data", meeting_data.create, auth.authenticateToken);
+router.post("/data/all", auth.authenticateToken, meeting_data.create);
 
 //Get all meeting_data
-router.get("/data", meeting_data.findAll);
+router.get("/data/all", meeting_data.findAll);
 
 //Get all meeting's meeting_data
 router.get("/:meetingId/data", meeting_data.findAllMeetingDataByMeeting);
@@ -33,6 +33,6 @@ router.delete("/:meetingId/data/:dataId",auth.authenticateToken, meeting_data.de
 router.delete("/:meetingId/data",auth.authenticateToken, meeting_data.deleteAllMeetingDataByMeeting);
 
 //Delete all meeting_data
-router.delete("/data", auth.authenticateToken, meeting_data.deleteAll);
+router.delete("/data/all", auth.authenticateToken, meeting_data.deleteAll);
 
 module.exports = router;
--- frontend/src/app/theboard/theboard.component.ts
+++ frontend/src/app/theboard/theboard.component.ts
@@ -3,34 +3,31 @@ import { Component, OnInit } from '@angular/core';
 declare var jQuery: any;
 declare var AwwBoard:any;
 declare var initToolbar: any;
+declare var $: any;
+export var aww : object;
 @Component({
   selector: 'app-theboard',
   templateUrl: './theboard.component.html',
   styleUrls: ['./theboard.component.css']
 })
 export class TheboardComponent implements OnInit {
-
   constructor() { }
-
   ngOnInit() {
-    (function ($) {
-      $(document).ready(function(){
-        var aww = new AwwBoard('#aww-wrapper', {
-          apiKey: '424db7a1-84c2-4cba-8154-f8d7a2ecaae4'
+      aww = new AwwBoard('#aww-wrapper', {
+      /* make sure you're using your own key here */
+      apiKey: '424db7a1-84c2-4cba-8154-f8d7a2ecaae4',
+    });
+     console.log("Type" + (typeof aww));
+    $.ajax({
+      'method': 'GET',
+      'url': 'https://awwapp.com/static/widget/sample_toolbar.html'
+    }).done(function(res, status) {
+      $('#aww-wrapper').append(res);
+      initToolbar();
+    });
 
 
-        });
-        $.ajax({
-          'method': 'GET',
-          'url': 'https://awwapp.com/static/widget/sample_toolbar.html'
-        }).done(function(res, status) {
-          console.log(res)
-          $('#aww-wrapper').append(res);
-          initToolbar();
-        })(jQuery);
-      })
-
-    })
   }
 
 }
+
